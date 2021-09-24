9a711331f5a924a06a19f8f733ac02f667871e78
Juliana Bicalho <julianabbicalho@gmail.com>
2021-09-24 10:50:45 -0300

    edit contact

--- enviaContato.php
+++ enviaContato.php
@@ -5,20 +5,22 @@ $email = $_POST["email"];
 $mensagem = $_POST["mensagem"];
 
 $texto = "
-<b>Nome:</b> $nome
-<b>E-mail:</b> $email
-<b>Mensagem:</b> $mensagem
+Nome: $nome
+E-mail: $email
+Mensagem: $mensagem
 ";
 
 // EMAIL PARA O DESTINATARIO
 $emailDestino = "julianabicalhodev@gmail.com";
-$headers .= "From:" . $email;
+$headers .= $nome . $email;
 mail($emailDestino, 'Teste de E-mail', $texto,);
 
 //EMAIL PARA O CLIENTE
-$headers2 .= "From:" . $email;
-$texto .= "Seu e-mail foi recebido e breve lhe darei um retorno!Muito obrigada pelo seu contato.";
-mail($email, 'RE:Teste de E-mail', $texto);
+$headers2 .= $nome . $email;
+$texto .= "Olá, tudo bem?";
+"Seu e-mail foi recebido e em breve lhe darei um retorno!";
+"Muito obrigada pelo seu contato.";
+mail($email, 'Mensagem enviada pelo site', $texto);
 
 ?>
 
